123bafff70de79006c0564ebe7e6bedbdf32675b
develoQ <develoQ.jp@gmail.com>
2022-11-23 21:27:55 +0900

    add xrpl.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,42 @@
+// const webpack = require('webpack');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   experimental: {
     appDir: true
-  }
+  },
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    const fallback = config.resolve.fallback || {};
+    Object.assign(fallback, {
+      "assert": require.resolve("assert"),
+      "bufferutil": require.resolve("bufferutil"),
+      "crypto": require.resolve("crypto-browserify"),
+      "http": require.resolve("stream-http"),
+      "https": require.resolve("https-browserify"),
+      "null-loader": require.resolve("null-loader"),
+      "os": require.resolve("os-browserify"),
+      "stream": require.resolve("stream-browserify"),
+      "url": require.resolve("url"),
+      "utf-8-validate": require.resolve("utf-8-validate"),
+      "ws": require.resolve('xrpl/dist/npm/client/WSWrapper'),
+    })
+    config.resolve.fallback = fallback
+    config.plugins = (config.plugins || []).concat([
+      new webpack.ProvidePlugin({
+        process: 'process/browser',
+        Buffer: ['buffer', 'Buffer']
+      })
+    ])
+
+    // This is deprecated in webpack 5 but alias false does not seem to work
+    config.module.rules.push({
+      test: /node_modules[\\\/]https-proxy-agent[\\\/]/,
+      use: 'null-loader',
+    })
+    return config;
+  },
 }
 
 module.exports = nextConfig
