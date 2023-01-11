68aa5f829c2e709f31e7c9596ccbb174d6882012
develoQ <develoQ.jp@gmail.com>
2023-01-11 23:22:40 +0900

    add header menu

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,51 @@
+import Link from 'next/link'
+
 export const Header = () => {
-  return <div></div>
+  const MenuList = () => {
+    return (
+      <>
+        <li>
+          <Link href='/'>Swap</Link>
+        </li>
+        <li>
+          <Link href='/tokens'>Tokens</Link>
+        </li>
+      </>
+    )
+  }
+  return (
+    <header className='navbar bg-base-100'>
+      <div className='navbar-start'>
+        <div className='dropdown'>
+          <label tabIndex={0} className='btn-ghost btn lg:hidden'>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              className='h-5 w-5'
+              fill='none'
+              viewBox='0 0 24 24'
+              stroke='currentColor'
+            >
+              <path strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='M4 6h16M4 12h8m-8 6h16' />
+            </svg>
+          </label>
+          <ul tabIndex={0} className='dropdown-content menu rounded-box menu-compact mt-3 w-52 bg-base-100 p-2 shadow'>
+            <MenuList />
+          </ul>
+        </div>
+        <Link href='/'>
+          <span className='btn-ghost btn text-xl normal-case'>xLedger</span>
+        </Link>
+      </div>
+      <div className='navbar-center hidden lg:flex'>
+        <ul className='menu menu-horizontal px-1'>
+          <MenuList />
+        </ul>
+      </div>
+      <div className='navbar-end'>
+        <Link href=''>
+          <span className='btn-primary btn'>Get started</span>
+        </Link>
+      </div>
+    </header>
+  )
 }
