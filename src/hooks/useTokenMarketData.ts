e4f6ebbee727f46baa452a1b39227cf610ef7f79
develoQ <develoQ.jp@gmail.com>
2023-01-15 18:12:50 +0900

    Revert "fix to fetch by serverside getTokensMarketData"
    This reverts commit feadd41a8578e6c3f714889d8afa34b5fa6b617d.

--- src/hooks/useTokenMarketData.ts
+++ src/hooks/useTokenMarketData.ts
@@ -1,13 +1,22 @@
-import { use, useMemo } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 
 import { usePagenation } from './usePagenation'
 
+import { TokensMarketData } from '@/@types/xrpl'
 import { getTokensMarketData } from '@/libs/xrpl'
 
 export const useTokenMarketData = () => {
+  const [tokensMarketData, setTokensMarketData] = useState<TokensMarketData[]>([])
   const { page, next, prev } = usePagenation()
   const perPage = 25
-  const tokensMarketData = use(getTokensMarketData({ page, per_page: perPage }))
+
+  useEffect(() => {
+    const f = async () => {
+      const data = await getTokensMarketData({ page, per_page: perPage })
+      setTokensMarketData(data)
+    }
+    f()
+  }, [page])
 
   const hasNextPage = useMemo(() => tokensMarketData.length === perPage, [tokensMarketData.length])
   const hasPrevPage = useMemo(() => page > 1, [page])
