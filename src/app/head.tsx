152eee20e0461932ff04aac9091ac05f2834eabb
develoQ <develoQ.jp@gmail.com>
2023-01-15 13:50:06 +0900

    add view port meta

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -2,6 +2,7 @@ const Head = () => {
   return (
     <>
       <title>xLedger Swap</title>
+      <meta name='viewport' content='initial-scale=1.0, width=device-width' />
       <meta name='description' content='Swap XRP Ledger assets' />
       <link rel='icon' href='/favicon.ico' />
     </>
