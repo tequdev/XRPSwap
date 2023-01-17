02fd6a2cab7f15ae1ca40ee037badc0c1bb5c1b5
develoQ <develoQ.jp@gmail.com>
2023-01-17 20:36:20 +0900

    add ogp

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -8,7 +8,7 @@ const Head = () => {
       <meta property='og:type' key='og:type' content='website' />
       <meta property='og:url' key='og:url' content={host} />
       <meta property='og:title' key='og:title' content='XRPSwap' />
-      <meta property='og:image' key='og:image' content={`${host}/ogp.png`} />
+      <meta property='og:image' key='og:image' content={`${host}/api/ogp/twitter-card`} />
       <meta name='og:description' key='og:description' content='Swap XRP Ledger tokens' />
       <meta name='twitter:card' key='twitter:card' content='summary_large_image' />
       <link rel='icon' href='/favicon.svg' />
