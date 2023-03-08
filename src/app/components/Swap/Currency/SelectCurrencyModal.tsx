0dcaea8c420c5940559ab39875be574c10994475
tequ <develoQ.jp@gmail.com>
2023-03-09 00:05:49 +0900

    fix No trustlines lost user #20

--- src/app/components/Swap/Currency/SelectCurrencyModal.tsx
+++ src/app/components/Swap/Currency/SelectCurrencyModal.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import { useRouter } from 'next/navigation'
 import { FC, useContext } from 'react'
 import { overrideTailwindClasses } from 'tailwind-override'
 import { Currency } from 'xrpl/dist/npm/models/common'
@@ -16,6 +17,7 @@ type Props = {
 }
 
 export const SelectCurrencyModal: FC<Props> = ({ onSelect, onClose }) => {
+  const router = useRouter()
   const { currencies: allCurrencies, loading } = useContext(TokenContext)
   const { currencies } = useContext(SwapContext)
   return (
@@ -34,32 +36,43 @@ export const SelectCurrencyModal: FC<Props> = ({ onSelect, onClose }) => {
             <div className='h-32 w-32 animate-spin rounded-full border-4 border-primary border-t-transparent' />
           </div>
         )}
-        {!loading &&
-          allCurrencies.map((data, index) => {
-            const currency = data.currency
-            const issuer = (data as any)?.issuer
-            const key = currency + issuer
-            const isUsed = [currencies.from, currencies.to].some(
-              (c) => c.currency === currency && (currency === 'XRP' || (c as any).issuer === issuer)
-            )
-            return (
-              <div
-                key={key}
-                className={overrideTailwindClasses(
-                  'my-1 flex gap-2 ' + (isUsed ? 'opacity-50' : 'cursor-pointer hover:md:bg-slate-100')
-                )}
-                onClick={() => (isUsed ? null : onSelect(data))}
-              >
-                <div className='flex h-12 w-12 items-center justify-center'>
-                  {data.icon && <Image src={data.icon} alt={data.name} unoptimized width={12 * 4} height={12 * 4} />}
+        {!loading && (
+          <>
+            {allCurrencies.map((data, index) => {
+              const currency = data.currency
+              const issuer = (data as any)?.issuer
+              const key = currency + issuer
+              const isUsed = [currencies.from, currencies.to].some(
+                (c) => c.currency === currency && (currency === 'XRP' || (c as any).issuer === issuer)
+              )
+              return (
+                <div
+                  key={key}
+                  className={overrideTailwindClasses(
+                    'my-1 flex gap-2 ' + (isUsed ? 'opacity-50' : 'cursor-pointer hover:md:bg-slate-100')
+                  )}
+                  onClick={() => (isUsed ? null : onSelect(data))}
+                >
+                  <div className='flex h-12 w-12 items-center justify-center'>
+                    {data.icon && <Image src={data.icon} alt={data.name} unoptimized width={12 * 4} height={12 * 4} />}
+                  </div>
+                  <div className='truncate'>
+                    <span className='block text-lg font-bold'>{data.name}</span>
+                    <span className='block text-xs text-gray-500'>{issuer}</span>
+                  </div>
                 </div>
-                <div className='truncate'>
-                  <span className='block text-lg font-bold'>{data.name}</span>
-                  <span className='block text-xs text-gray-500'>{issuer}</span>
-                </div>
+              )
+            })}
+            <div
+              className={overrideTailwindClasses('my-1 flex gap-2 cursor-pointer hover:md:bg-slate-100')}
+              onClick={() => router.push('/tokens')}
+            >
+              <div className='flex h-12 w-full items-center justify-center truncate'>
+                <span className='block text-lg font-semibold text-current'>Find new tokens→</span>
               </div>
-            )
-          })}
+            </div>
+          </>
+        )}
       </div>
     </div>
   )
